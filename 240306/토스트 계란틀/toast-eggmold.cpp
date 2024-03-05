0bfcf299939362e6380e4f6ee9b125e7544123f3
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-03-06 02:11:42 +0900

    Title: 토스트 계란틀, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240306/토스트 계란틀/toast-eggmold.cpp
+++ 240306/토스트 계란틀/toast-eggmold.cpp
@@ -27,72 +27,57 @@ int main() {
     }
 
     while(1) {
-        vector<pair<vector<pair<int,int>>, int>> eggmemories;
-
+        int isEnd = 1;
+        // 시작 지점 찾기
+        int starty = -1, startx = -1;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
-                visited[i][j] = 0;
-            }
-        }
+                if(visited[i][j] == 0) {
+                    starty = i; startx = j;
 
-        while(1) {
-            // 시작 지점 찾기
-            int starty = -1, startx = -1;
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    if(visited[i][j] == 0) {
-                        starty = i; startx = j;
-                        break;
+                    // 시작지점에서 하나로 연결된 지점들 탐색
+                    visited[starty][startx] = 1;
+                    queue<pair<int, int>> q;
+                    vector<pair<int, int>> eggmemory;
+                    q.push({starty, startx});
+                    eggmemory.push_back({starty, startx});
+
+                    int totalegg = map[starty][startx];
+                    while(!q.empty()) {
+                        auto curr = q.front();
+                        q.pop();
+
+                        for(int i = 0; i < 4; i++) {
+                            int nexty = curr.first + dir[i][0];
+                            int nextx = curr.second + dir[i][1];
+
+                            if(visited[nexty][nextx] == 1) continue;
+                            if(nexty < 0 || nexty >= n || nextx < 0 || nextx >= n) continue;
+                            int diff = abs(map[curr.first][curr.second] - map[nexty][nextx]);
+                            if(diff < l || diff > r) continue;
+
+                            visited[nexty][nextx] = 1;
+                            totalegg += map[nexty][nextx];
+                            q.push({nexty, nextx});
+                            eggmemory.push_back({nexty, nextx});
+                        }
+                    }
+                    
+                    if(eggmemory.size() > 1) {
+                        int sum = 0;
+                        for(auto pos : eggmemory) {
+                            sum += map[pos.first][pos.second];
+                        }
+                        for(auto pos : eggmemory) {
+                            map[pos.first][pos.second] = sum/eggmemory.size();
+                        }
+                        isEnd = 0;
                     }
                 }
             }
-            if(starty == -1 || startx == -1) break;
-
-            // 시작지점에서 하나로 연결된 지점들 탐색
-            visited[starty][startx] = 1;
-            queue<pair<int, int>> q;
-            vector<pair<int, int>> eggmemory;
-            q.push({starty, startx});
-            eggmemory.push_back({starty, startx});
-
-            int totalegg = map[starty][startx];
-            while(!q.empty()) {
-                auto curr = q.front();
-                q.pop();
-
-                for(int i = 0; i < 4; i++) {
-                    int nexty = curr.first + dir[i][0];
-                    int nextx = curr.second + dir[i][1];
-
-                    if(visited[nexty][nextx] == 1) continue;
-                    if(nexty < 0 || nexty >= n || nextx < 0 || nextx >= n) continue;
-                    int diff = abs(map[curr.first][curr.second] - map[nexty][nextx]);
-                    if(diff < l || diff > r) continue;
-
-                    visited[nexty][nextx] = 1;
-                    totalegg += map[nexty][nextx];
-                    q.push({nexty, nextx});
-                    eggmemory.push_back({nexty, nextx});
-                }
-            }
-            eggmemories.push_back({eggmemory, totalegg});
         }
-
-        if(eggmemories.size() == n*n) {
-            break;
-        }
-
-        // 각 구역별로 egg 균등하게 분배
-        // cout << '\n';
-        for(auto eggmemory : eggmemories) {
-            int tot = eggmemory.second;
-            // cout << tot << '\n';
-            for(auto egg : eggmemory.first) {
-                map[egg.first][egg.second] = tot/eggmemory.first.size();
-                // cout << map[egg.first][egg.second] << '\n';
-            }
-        }
-        answer++;
+        if(isEnd == 0) answer++;
+        else break;
     }
 
     cout << answer;
