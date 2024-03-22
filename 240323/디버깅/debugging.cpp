8021eaf377a9f9b3777eef2bd93cb27f8e94ac9e
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-03-23 00:53:57 +0900

    Title: 디버깅, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

--- 240323/디버깅/debugging.cpp
+++ 240323/디버깅/debugging.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int n, h, m;
 
+int answer = -1;
+
 vector<vector<int>> map(35, vector<int> (15));
 vector<pair<int,int>> memoryleakCandidates;
 vector<pair<int,int>> addedMemoryLeak;
@@ -13,8 +15,43 @@ vector<vector<pair<int,int>>> addedMemoryLeaks;
 
 // n-1 * h 만큼의 예비 유실선에서 뽑는다
 void addMemoryLeaks(int idx, int size) {
+    if(answer != -1) {
+        return;
+    }
+
     if(addedMemoryLeak.size() == size) {
-        addedMemoryLeaks.push_back(addedMemoryLeak);
+        for(auto ml : addedMemoryLeak) {
+            map[ml.first][ml.second] = 1;
+        }
+
+        int success = 0;
+
+        // 각 시작점으로부터 사다리 게임
+        for(int i = 1; i <= m; i++) { // 고객
+            int currentm = i;
+            int currenth = 1;
+            while(currenth <= h) {
+                if(map[currenth][currentm] == 1){
+                    currentm++;
+                } // 우로 이동
+                else if(map[currenth][currentm-1] == 1) {
+                    currentm--;
+                } // 좌로 이동
+
+                currenth++;
+            }
+
+            if(i == currentm) {
+                success++;
+            }
+        }
+        if(success == m) {
+            answer = addedMemoryLeak.size();
+        }
+
+        for(auto ml : addedMemoryLeak) {
+            map[ml.first][ml.second] = 0;
+        }
         return;
     }
 
@@ -46,49 +83,10 @@ int main() {
         }
     }
 
-    addMemoryLeaks(0, 0);
-    addMemoryLeaks(0, 1);
-    addMemoryLeaks(0, 2);
-    addMemoryLeaks(0, 3);
-
-    int answer = -1;
-
-    for(auto memoryleaks : addedMemoryLeaks) {
-        for(auto ml : memoryleaks) {
-            map[ml.first][ml.second] = 1;
-        }
-
-        int success = 0;
-
-        // 각 시작점으로부터 사다리 게임
-        for(int i = 1; i <= m; i++) { // 고객
-            int currentm = i;
-            int currenth = 1;
-            while(currenth <= h) {
-                if(map[currenth][currentm] == 1){
-                    currentm++;
-                } // 우로 이동
-                else if(map[currenth][currentm-1] == 1) {
-                    currentm--;
-                } // 좌로 이동
-
-                currenth++;
-            }
-
-            if(i == currentm) {
-                success++;
-            }
-        }
-        if(success == m) {
-            answer = memoryleaks.size();
-            break;
-        }
-
-        for(auto ml : memoryleaks) {
-            map[ml.first][ml.second] = 0;
-        }
-
+    for(int i = 0; i < 4; i++) {
+        addMemoryLeaks(0, i);
     }
+
     cout << answer;
 
     return 0;
