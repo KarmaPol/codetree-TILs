5a73445541792065041e29d9ad7778d7f814bb83
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-13 04:06:11 +0900

    Title: 꼬리잡기놀이, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240413/꼬리잡기놀이/tail-catch-play.cpp
+++ 240413/꼬리잡기놀이/tail-catch-play.cpp
@@ -157,6 +157,10 @@ void throwBall(int round) {
 }
 
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     cin >> n >> m >> k;
 
     for(int i = 0; i < n; i++) {
