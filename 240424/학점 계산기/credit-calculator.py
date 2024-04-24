4aa8701e6dd26ec4aea5dd9b24fe483d2ae8ddba
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-24 15:30:45 +0900

    Title: 학점 계산기, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240424/학점 계산기/credit-calculator.py
+++ 240424/학점 계산기/credit-calculator.py
@@ -2,12 +2,12 @@ n = int(input());
 subjects = list(map(float,input().split()))
 
 tot = sum(subjects)
+avg = round(tot/n, 1)
+print(avg)
 
-print(round(tot/n, 1))
-
-if(tot >= 3.0):
-    print('Good')
-elif(tot >= 4.0):
+if(avg >= 4.0):
     print('Perfect')
+elif(avg >= 3.0):
+    print('Good')
 else:
     print('Poor')
