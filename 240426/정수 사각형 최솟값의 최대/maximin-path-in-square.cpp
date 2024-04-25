9513f56d7760a5f1143cb6f899ec6f457f070545
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 00:21:47 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

--- 240426/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240426/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -1,54 +1,42 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int dp[105][105];
-int map[105][105];
-
 int n;
+int num[1005][1005];
+int dp[1005][1005];
 
-int dy[2] = {1, 0};
-int dx[2] = {0, 1};
+void Initialize() {
+    dp[0][0] = num[0][0];
 
+    for(int i = 1; i < n; i++) {
+        dp[i][0] = min(dp[i-1][0], num[i][0]);
+    }
+
+    for(int j = 1; j < n; j++) {
+        dp[0][j] = min(dp[0][j-1], num[0][j]);
+    }
+}
+ 
 int main() {
     cin >> n;
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            cin >> map[i][j];
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> num[i][j];
         }
     }
 
-    for(int i = 0; i <= n; i++) {
-        dp[0][i] = -2e9;
-        dp[i][0] = -2e9;
-    }
+    Initialize();
 
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            int currenty = i, currentx = j;
-
-            int prevmin = -2e9;
-            for(int d = 0; d < 2; d++) {
-                int prevy = currenty - dy[d];
-                int prevx = currentx - dx[d];
-                prevmin = max(dp[prevy][prevx], prevmin);
-            }
-            if(prevmin == -2e9) prevmin *= -1;
-
-            prevmin = min(prevmin, map[currenty][currentx]);
-            dp[currenty][currentx] = prevmin;
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < n; j++) {
+            dp[i][j] = min(max(dp[i-1][j], dp[i][j-1]), num[i][j]);
         }
-    } 
-
-    // for(int i = 1; i <= n; i++) {
-    //     for(int j = 1; j <= n; j++) {
-    //         cout << dp[i][j];
-    //     }
-    //     cout << '\n';
-    // }
-
-    cout << dp[n][n];
+    }
+    
+    cout << dp[n-1][n-1];
 
     return 0;
 }
