6049d796655324fee03121c197ab1e999f4e3c40
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-25 22:22:28 +0900

    Title: 서로 다른 BST 개수 세기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240425/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
+++ 240425/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -8,7 +8,9 @@ int memo[25];
 int topDown(int n) {
     if(memo[n] != 0) return memo[n];
 
-    memo[n] = (n-2) * topDown(n-1) + n;
+    int prev = (n-3) * topDown(n-1);
+    if(prev < 0) prev = 0;
+    memo[n] = prev + (n*(n+1)/2 -1);
     return memo[n];
 }
 
