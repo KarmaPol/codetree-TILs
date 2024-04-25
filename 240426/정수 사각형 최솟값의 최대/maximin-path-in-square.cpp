462c01ba16d91d2ea10dc8ac2b7cc1b7d2c25dc6
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 00:15:15 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

--- 240426/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240426/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -20,8 +20,8 @@ int main() {
     }
 
     for(int i = 0; i <= n; i++) {
-        dp[0][i] = 2e9;
-        dp[i][0] = 2e9;
+        dp[0][i] = -2e9;
+        dp[i][0] = -2e9;
     }
 
     for(int i = 1; i <= n; i++) {
@@ -34,11 +34,20 @@ int main() {
                 int prevx = currentx - dx[d];
                 prevmin = max(dp[prevy][prevx], prevmin);
             }
+            if(prevmin == -2e9) prevmin *= -1;
 
             prevmin = min(prevmin, map[currenty][currentx]);
             dp[currenty][currentx] = prevmin;
         }
     } 
+
+    // for(int i = 1; i <= n; i++) {
+    //     for(int j = 1; j <= n; j++) {
+    //         cout << dp[i][j];
+    //     }
+    //     cout << '\n';
+    // }
+
     cout << dp[n][n];
 
     return 0;
