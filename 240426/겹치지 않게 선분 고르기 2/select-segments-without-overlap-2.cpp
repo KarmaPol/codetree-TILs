44a706871caebb02e03c6faa64bc761bf56e9409
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 21:04:34 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240426/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int n;
+
+vector<pair<int,int>> lines;
+int dp[1005]; // 선분 개수
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int start, end;
+        cin >> start >> end;
+        lines.push_back({start, end});
+    }
+    
+    sort(lines.begin(), lines.end());
+
+    dp[0] = 1;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(lines[i].first <= lines[j].second) continue;
+            
+            dp[i] = max(dp[j]+1, dp[i]);
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans;
+
+    return 0;
+}
