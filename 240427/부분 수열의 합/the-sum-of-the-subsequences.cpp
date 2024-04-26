4caaee3422a390a454bafacd95f4a4c0ef5a43f3
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-27 01:25:00 +0900

    Title: 부분 수열의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240427/부분 수열의 합/the-sum-of-the-subsequences.cpp
+++ 240427/부분 수열의 합/the-sum-of-the-subsequences.cpp
@@ -12,19 +12,21 @@ int main() {
 
     for(int i = 0; i < n; i++) {
         cin >> arr[i];
+        dp[i] = -1;
     }
 
+    dp[0] = 0;
     for(int i = 0; i < n; i++) {
         int currnum = arr[i];
         for(int j = m; j >= 0; j--) {
             if(currnum > j) continue;
-
+            if(dp[j-currnum] == -1) continue;
             dp[j] = max(dp[j], dp[j- currnum] + 1);
         }
     }
 
     string ans = "Yes";
-    if(dp[m] == 0)
+    if(dp[m] == -1)
         ans = "No";
     
     cout << ans;
