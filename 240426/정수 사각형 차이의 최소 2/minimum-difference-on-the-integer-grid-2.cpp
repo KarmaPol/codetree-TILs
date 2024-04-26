fe4d73dc290c6893159156c7de57c8c039cd00b2
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 17:21:45 +0900

    Title: 정수 사각형 차이의 최소 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240426/정수 사각형 차이의 최소 2/minimum-difference-on-the-integer-grid-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <tuple>
+
+using namespace std;
+
+int n;
+int map[105][105];
+tuple<int,int,int> dp[105][105];
+
+int dy[2] = {0, 1};
+int dx[2] = {1, 0};
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> map[i][j];
+            dp[i][j] = make_tuple(map[i][j], map[i][j], 2e9);
+        }
+    }
+
+    dp[0][0] = make_tuple(map[0][0], map[0][0], 0);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            int currenty = i, currentx = j;
+            int currentMax, currentMin, currentDiff;
+            tie(currentMax, currentMin, currentDiff) = dp[currenty][currentx];
+
+            for(int d = 0; d < 2; d++) {
+                int nexty = currenty + dy[d];
+                int nextx = currentx + dx[d];
+                int nextMax, nextMin, nextDiff;
+                tie(nextMax, nextMin, nextDiff) = dp[nexty][nextx];
+
+                int tempMax = max(map[nexty][nextx], currentMax);
+                int tempMin = min(map[nexty][nextx], currentMin);
+                int tempDiff = tempMax - tempMin;
+                if(tempDiff < nextDiff) {
+                    dp[nexty][nextx] = make_tuple(tempMax,tempMin, tempDiff);
+                }
+            }
+        }
+    }
+    
+    cout << get<2>(dp[n-1][n-1]);
+
+    return 0;
+}
