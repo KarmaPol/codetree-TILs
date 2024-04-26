a68f96c8b9f891dbe7ca8c75e2f43809b273bfcb
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 19:18:58 +0900

    Title: 최대 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240426/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int numbers[1005];
+int dp[1005];
+
+int main() {
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    dp[0] = 1;
+    for(int i = 1; i < n; i++) {
+        for(int j = 0; j < i; j++) {
+            if(numbers[i] > numbers[j])
+                dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; i++)
+        ans = max(ans, dp[i]);
+
+    cout << ans;
+
+    return 0;
+}
