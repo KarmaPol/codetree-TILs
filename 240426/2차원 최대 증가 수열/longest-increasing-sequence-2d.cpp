fef220d540a31c90abe6e642d7ae7c19468b21ac
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 20:45:36 +0900

    Title: 2차원 최대 증가 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240426/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 240426/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -36,7 +36,7 @@ int main() {
 
     int ans = 0;
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+        for(int j = 0; j < m; j++) {
             ans = max(ans, dp[i][j]);
         }
     }
