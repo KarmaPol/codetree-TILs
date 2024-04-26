f65619d632e1361bec558722fcc41ccd2a7db919
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-26 20:21:12 +0900

    Title: 최대 점프 횟수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240426/최대 점프 횟수/maximum-number-of-jumps.cpp
+++ 240426/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -12,16 +12,25 @@ int main() {
     
     for(int i = 0; i < n; i++) {
         cin >> map[i];
+        dp[i] = -1;
     }
 
+    dp[0] = 0;
+
     for(int i = 1; i < n; i++) {
-        for(int j = 0; j < i; j++) {
+        for(int j = 0; j < i; j++) { 
             if(i > map[j] + j) continue;
+            if(dp[j] == -1) continue;
+
             dp[i] = max(dp[j] + 1, dp[i]);
         }
     }
 
-    cout << dp[n-1];
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        ans = max(ans, dp[i]);
+    }
+    cout << ans;
 
     return 0;
 }
