36924299626448196c2882dce7439ab3dea85c15
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-04-27 01:08:25 +0900

    Title: 최대 동전 거슬러주기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240427/최대 동전 거슬러주기/max-coin-change.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int coins[105];
+int dp[10005];
+
+int main() {
+    cin >> n >> m;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    for(int i = 1; i <= m; i++) {
+        for(int j = 0; j < n; j++) {
+            int currentcoin = coins[j];
+
+            if(currentcoin > i) continue;
+            dp[i] = max(dp[i], dp[i - currentcoin] + 1);
+        }
+    }
+
+    cout << dp[m];
+
+    return 0;
+}
