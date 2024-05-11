d1f485f17879acd643a32064e7d981a569c52a3f
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-05-11 20:08:00 +0900

    Title: 동일하게 분할, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240511/동일하게 분할/equal-partition.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int numbers[105];
+bool dp[105][100005];
+
+int main() {
+    
+    cin >> n;
+
+    int tot = 0;
+
+    for(int i = 1; i <= n; i++) {
+        cin >> numbers[i];
+        tot += numbers[i];
+    }
+
+    dp[0][0] = true;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 0; j <= tot; j++) {
+            if(j >= numbers[i]) {
+                if(dp[i-1][j-numbers[i]]) {
+                    dp[i][j] = true;
+                }
+            }
+            
+            if(dp[i-1][j]) dp[i][j] = true;
+        }
+    }
+
+    string ans = "No";
+    for(int i = 1; i <= tot; i++) {
+        // cout << dp[n][i] << '\n';
+        if(dp[n][i] == true && dp[n][i*2] == true) {
+            ans = "Yes";
+            break;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
