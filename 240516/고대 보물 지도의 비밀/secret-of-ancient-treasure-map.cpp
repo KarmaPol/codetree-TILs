9471af822404796620507770425665d3a56f7fed
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-05-16 23:28:37 +0900

    Title: 고대 보물 지도의 비밀, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240516/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int n, k;
+
+int numbers[100005];
+int dp[100005][15];
+
+int main() {
+
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) cin >> numbers[i];
+
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= k; j++) {
+            dp[i][j] = -2e9;
+        }
+    }
+
+    dp[0][0] = 0;
+
+    for(int i = 0; i <= n; i++) {
+        for(int j = 0; j < k; j++) {
+            // if(dp[i][j] == -2e9) continue; 
+            if(numbers[i+1] < 0) {
+                dp[i+1][j+1] = max(dp[i][j+1], dp[i][j] + numbers[i+1]);
+                dp[i+1][1] = max(dp[i+1][1], numbers[i+1]);
+            } 
+            else {
+                dp[i+1][j] = max(dp[i][j], dp[i][j] + numbers[i+1]);
+                dp[i+1][0] = max(dp[i][j], numbers[i+1]);
+            }
+        }
+    }
+
+    int ans = -2e9;
+    for(int j = 1; j <= n; j++) {
+        for(int i = 0; i <= k; i++) {
+            ans = max(ans, dp[j][i]);
+        }
+    }
+    cout << ans;
+
+    return 0;
+}
