7170cb4c73ed8aacc437f1bd98df415dcffdf034
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-05-18 00:03:52 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240518/올바른 등식 만들기/right-equality.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int OFFSET = 30;
+int numbers[105];
+int dp[60][105];
+
+int main() {
+
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++) {
+        cin >> numbers[i];
+    }
+
+    dp[30][0] = 1;
+
+    for(int j = 1; j <= n; j++) {
+        for(int i = -20; i <= 20; i++) {
+            if(dp[i+OFFSET - numbers[j]][j-1]){
+                dp[i+OFFSET][j] += dp[i+OFFSET - numbers[j]][j-1];
+            }
+            if(dp[i+OFFSET + numbers[j]][j-1]){
+                dp[i+OFFSET][j] += dp[i+OFFSET + numbers[j]][j-1];
+            }
+        }
+    }
+    // for(int j = 0; j <= n; j++) {
+    //     for(int i = 10; i <= 50; i++) {
+    //         cout << i-OFFSET << ": " << dp[i][j] << '\n'; 
+    //     }
+    //     cout << '\n';
+    // }
+    
+    cout << dp[m+OFFSET][n];
+
+    return 0;
+}
