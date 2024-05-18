145d777bc25b80a9d0a658413888b798fa8b44e9
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-05-18 23:31:44 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240518/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
+++ 240518/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 int n;
 
-int dp[1005][3][3];
+int dp[1005][5][5];
 
-int MOD = 1e9+7;
-
+const int MOD = 1e9 + 7;
 int main() {
     cin >> n;
 
-    dp[1][0][0] = 1;
-    dp[1][1][0] = 1;
-    dp[1][0][1] = 1;
+    dp[1][1][0] = 1;  // 첫 번째 날에 T를 받은 경우
+    dp[1][0][1] = 1;  // 첫 번째 날에 B를 받은 경우
+    dp[1][0][0] = 1;  // 첫 번째 날에 G를 받은 경우
 
     for(int i = 1; i < n; i++) {
         for(int j = 0; j < 3; j++) {
             for(int k = 0; k < 3; k++) {
+                // 다음 날로 넘어가는 경우의 수를 갱신합니다.
                 dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % MOD;
-                dp[i + 1][j][0] = (dp[i+1][j][0] + dp[i][j][k]) % MOD;
-                dp[i+1][j][k+1] = (dp[i+1][j][k+1] + dp[i][j][k]) % MOD;
+                dp[i + 1][j][0] = (dp[i + 1][j][0] + dp[i][j][k]) % MOD;
+                dp[i + 1][j][k + 1] = (dp[i + 1][j][k + 1] + dp[i][j][k]) % MOD;
             }
         }
     }
 
+    // 최종 결과를 계산합니다.
     int ans = 0;
-    for(int i = 0; i < 3; i++) {
-        for(int j = 0; j < 3; j++) {
-            ans += dp[n][i][j];
+    for(int j = 0; j < 3; j++) {
+        for(int k = 0; k < 3; k++) {
+            ans = (ans + dp[n][j][k]) % MOD;
         }
     }
 
