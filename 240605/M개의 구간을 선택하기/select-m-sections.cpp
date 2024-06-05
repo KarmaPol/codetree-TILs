0097fe271ff4f5175445196ee0c05b8fb7318a11
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-06-05 16:57:06 +0900

    Title: M개의 구간을 선택하기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240605/M개의 구간을 선택하기/select-m-sections.cpp
+++ 240605/M개의 구간을 선택하기/select-m-sections.cpp
@@ -23,7 +23,7 @@ int main() {
     }
 
     for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= m && j <= i; j++) {
+        for(int j = 1; j <= m; j++) {
             // 1개전까지의 구간합
             if(dp[i-1][j] != -2e9)
                 dp[i][j] = max(dp[i][j], dp[i-1][j]);
