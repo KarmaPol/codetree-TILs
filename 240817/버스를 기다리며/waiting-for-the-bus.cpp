cd266ee94b9644f8723ae22723d2602b8845cc8a
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-08-17 02:25:36 +0900

    Title: 버스를 기다리며, Time: 24ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240817/버스를 기다리며/waiting-for-the-bus.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n, m ,c;
+int members[100005];
+
+bool isPossible(int target) {
+    int busCount = 1, userCount = 1;
+    int last = members[0];
+
+    for(int i = 1; i < n; i++) {
+        int current = members[i];
+
+        if(userCount > c) {
+            last = current;
+            busCount++;
+            userCount = 1;
+        }
+
+        if(current - last > target) {
+            last = current;
+            busCount++;
+            userCount++;
+        }
+    }
+
+    return m >= busCount;
+}
+
+int main() {
+    cin >> n >> m >> c;
+
+    for(int i = 0; i < n; i++) {
+        cin >> members[i];
+    }
+
+    int left = 0, right = 1e9, mid;
+    int answer = 1e9;
+
+    while(left <= right) {
+        mid = (left + right) / 2;
+
+        if(isPossible(mid)) {
+            answer = min(answer, mid);
+            right = mid - 1;
+        }
+        else {
+            left = mid + 1;
+        }
+    }
+
+    cout << answer;
+    
+    return 0;
+}
