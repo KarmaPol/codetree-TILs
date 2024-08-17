81ccb603902f90eab69b94e64cff647bc7fa98bc
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-08-18 01:43:39 +0900

    Title: 동전 더하기, Time: 1043ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240818/동전 더하기/add-coins.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int n, k;
+
+int coins[15];
+
+int main() {
+
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++) {
+        cin >> coins[i];
+    }
+
+    int coinCount = 0, coinIndex = n-1;
+    while(k > 0) {
+        if(coins[coinIndex] > k) continue;
+
+        coinCount += k/coins[coinIndex];
+        k = k % coins[coinIndex];
+        coinIndex--;
+    }
+
+    cout << coinCount;
+
+    return 0;
+}
