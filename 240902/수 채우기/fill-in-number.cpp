28308818b2f1d6ba72ef3f9ba3b5b687175c990b
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-09-02 22:38:57 +0900

    Title: 수 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240902/수 채우기/fill-in-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+int main() {
+    cin >> n;
+
+    int count = 0;
+
+    int fiveM = n/5; 
+    if(n % 2 == 1) {
+        fiveM -= 1;
+    }
+    count += fiveM; n -= fiveM * 5;
+    count += n / 2; n -= (n/2) * 2;
+
+    if(n != 0) {
+        count = -1;
+    }
+    cout << count;
+
+    return 0;
+}
