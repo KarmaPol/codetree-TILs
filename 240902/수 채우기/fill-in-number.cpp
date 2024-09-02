907b10e78485a1071eca7607c8cc004d8e953c8a
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-09-02 22:42:36 +0900

    Title: 수 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240902/수 채우기/fill-in-number.cpp
+++ 240902/수 채우기/fill-in-number.cpp
@@ -9,10 +9,11 @@ int main() {
     int count = 0;
 
     int fiveM = n/5; 
-    if(n >= 5 && n % 2 == 1) {
-        fiveM--;
-    }
     count += fiveM; n -= fiveM * 5;
+    if(count > 0 && n % 2 == 1) {
+        n += 5;
+        count--;
+    }
     count += n / 2; n -= (n/2) * 2;
 
     if(n != 0) {
