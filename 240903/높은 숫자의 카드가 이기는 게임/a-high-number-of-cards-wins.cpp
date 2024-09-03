84915675c3755058bab996623757324694a7abeb
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-09-03 23:39:16 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 28ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
+++ 240903/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.cpp
@@ -4,18 +4,24 @@ using namespace std;
 
 int n;
 int cardB[50005];
+int isPicked[100005];
 
 int main() {
     cin >> n;
 
     for(int i = 0; i < n; i++) {
-        cin >> cardB[i];
+        int card;
+        cin >> card;
+        cardB[i] = card;
+        isPicked[card] = 1;
     }
 
     sort(cardB, cardB + n);
 
     int bidx = 0, answer = 0;
     for(int i = 1; i <= 2*n; i++) {
+        if(isPicked[i]) continue;
+
         if(i > cardB[bidx]) {
             bidx++; answer++;
             if(bidx >= n) break;
