fc00f5f96622d957bf795142b38da3483bb091a6
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-09-17 22:11:12 +0900

    Title: 소괄호 매칭 수 최대화하기, Time: 7ms, Memory: 3MB, Status: Wrong Answer - CodeTree

---
+++ 240917/소괄호 매칭 수 최대화하기/maximize-the-number-of-parenthesis-matches.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n;
+
+string inputs[100005];
+
+bool cmp(string a, string b) {
+    if(a == ")" && b[0] == ')') {
+        return a < b;
+    }
+    if(b == ")" && a[0] == ')') {
+        return a > b;
+    }
+    return a < b;
+}
+
+int main() {
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        cin >> inputs[i];
+    }
+
+    sort(inputs, inputs+n, cmp);
+
+    string t;
+    for(int i = 0; i < n; i++) {
+        // cout << inputs[i] << '\n';
+        t += inputs[i];
+    }
+
+    int leftCount = 0, answer = 0;
+
+    cout << t << '\n';
+    for(int i = 0; i < t.size(); i++) {
+        if(t[i] == '(') {
+            leftCount++;
+        }
+
+        else if(t[i] == ')') {
+            answer += leftCount;
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
