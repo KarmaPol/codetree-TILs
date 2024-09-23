991e982826c73846561ae768463e68d5b73ee4bc
Kyunghun Kim <86098663+KarmaPol@users.noreply.github.com>
2024-09-24 00:36:15 +0900

    Title: 양수 직사각형의 최대 크기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int n, m;
+
+int map[25][25];
+
+bool isAllPositive(int firstY, int firstX, int secondY, int secondX) {
+    int lessY = min(firstY, secondY);
+    int greaterY = max(firstY, secondY);
+
+    int lessX = min(firstX, secondX);
+    int greaterX = max(firstX, secondX);
+
+    for(int i = lessY; i <= greaterY; i++) {
+        for(int j = lessX; j <= greaterX; j++) {
+            if(map[i][j] < 1) return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    cin >> n >> m;
+
+    int maxSize = 0;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            int firstY = i, firstX = j;
+
+            for(int p = 0; p < n; p++) {
+                for(int q = 0; q < m; q++) {
+                    int secondY = p, secondX = q;
+
+                    if(firstY == secondY || firstX == secondX) continue;
+
+                    if(!isAllPositive(firstY, firstX, secondY, secondX)) continue;
+                    // cout << firstY << " " << firstX << " " << secondY << " " << secondX << '\n';
+                    maxSize = max(maxSize, (abs(secondY-firstY)+1)*(abs(secondX-firstX)+1));
+                }
+            }
+        }
+    }
+
+    cout << maxSize;
+
+    return 0;
+}
